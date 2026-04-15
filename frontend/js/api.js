88c56f1a5c0da487c5e6bb8b758d584ca9d1f92a
Madhucharan Durgam <durgammadhucharan.131650@marwadiuniversity.ac.in>
2026-04-15 16:41:06 +0530

    Update api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api"
+const API_URL = "https://resume-analyzing-tool.onrender.com/api"
 
 async function registerUser(data) {
   const res = await fetch(`${API_URL}/auth/register`, {
